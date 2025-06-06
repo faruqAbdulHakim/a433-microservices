d4b621999676448e630069e9b78da55677549b9c
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2025-06-06 11:17:11 +0700

    feat: add Dockerfile, build and push bash script

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use node:18-alpine as image
+FROM node:18-alpine
+
+# Set work directory
+WORKDIR /app
+
+# Copy paste package.json and package-lock.json into work directory
+COPY . .
+
+# Install node dependencies with clean install
+RUN npm ci
+
+# Add bash
+RUN apk add --no-cache bash
+
+# Get wait-for-it bash script
+RUN wget -O /bin/wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+
+# Make it executable
+RUN chmod +x /bin/wait-for-it.sh
+
+# Run index.js
+CMD ["node", "index.js"]
+ 
+# Expose port 3001
+EXPOSE 3001
---
+++ build_and_push_image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the docker image name
+DOCKER_IMAGE_NAME="shipping-service"
+
+# Set the docker image tag
+DOCKER_IMAGE_TAG="latest"
+
+# Set the docker image name with github packages format
+GHCR_IMAGE_NAME="ghcr.io/faruqabdulhakim/a433-microservices/$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG"
+
+# Create a docker image from this project
+docker build -t $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG .
+
+# Prepare a docker image for github package
+docker tag $DOCKER_IMAGE_NAME:$DOCKER_IMAGE_TAG $GHCR_IMAGE_NAME
+
+# Login to github Packages (already set Personal Access Token in GitHub)
+echo $CR_PAT | docker login ghcr.io -u faruqAbdulHakim --password-stdin
+
+# Push/upload the image to docker hub
+docker push $GHCR_IMAGE_NAME
